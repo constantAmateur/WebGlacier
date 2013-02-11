a4199f001cb8a68a8c60a0b26743707292bfa360
constantAmateur <theconstantamateur@gmail.com>
2013-02-11 10:57:01 +0000

    Update setup.py
    Make setup script more sensible

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Web Glacier",
-    version="1.0",
-    long_description="A simple web front-end for interfacing with Amazon Glacier.  The information about what is stored on Amazon's servers is stored locally in a database.",
+    name="WebGlacier",
+    version="0.1.0.alpha",
+    long_description="A simple web front-end for interfacing with Amazon Glacier.",
     inclued_package_data=True,
     zip_safe=False,
     install_requires=[
       "Flask",
       "flask-sqlalchemy",
-      "boto",
-      "mysql-python",
-      "distribute>=0.6.28",
+      "flask-wtf",
+      "boto"
       ],
     packages=find_packages()
     )
