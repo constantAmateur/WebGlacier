9c3a32730b427077b3f9b9a52683f20b9fd9e1eb
Matthew Young <constantAmateur@gmail.com>
2015-01-13 00:28:49 +0000

    More fixes

--- WebGlacier/api/vault_methods.py
+++ WebGlacier/api/vault_methods.py
@@ -11,7 +11,7 @@ import tempfile,os
 from werkzeug import Headers
 
 #Flask imports
-from flask import request, redirect, url_for, abort, Response
+from flask import request, redirect, url_for, abort, Response, stream_with_context
 
 #WebGlacier imports
 import WebGlacier as WG
